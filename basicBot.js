1213e71122e78566f7904187d548d834bba98198
Corran <corranharker@hotmail.co.uk>
2015-04-21 19:05:55 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2677,15 +2677,15 @@
                 }
             },
 
-            reloadCommand: {
-                command: 'reload',
+            restartCommand: {
+                command: 'restart',
                 rank: 'host',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat(basicBot.chat.reload);
+                        API.sendChat(basicBot.chat.restart);
                         storeToStorage();
                         basicBot.disconnectAPI();
                         kill();
