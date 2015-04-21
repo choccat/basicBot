365fb54747becfd8c78e6329b953aaf6ed56d4a9
Corran <corranharker@hotmail.co.uk>
2015-04-21 17:45:20 +0100

    Update basicBot.js
    Restart command

--- basicBot.js
+++ basicBot.js
@@ -2726,6 +2726,25 @@
                     }
                 }
             },
+            
+            restartCommand: {
+                command: 'restart',
+                rank: 'bouncer',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.restart);
+                        storeToStorage();
+                        basicBot.disconnectAPI();
+                        kill();
+                        setTimeout(function () {
+                            $.getScript(basicBot.scriptLink);
+                        }, 2000);
+                    }
+                }
+            },
 
             restrictetaCommand: {
                 command: 'restricteta',
