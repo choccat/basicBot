be0e8c4cf41ded91d7d9baa97daf5981cee0dab0
Corran <corranharker@hotmail.co.uk>
2015-04-26 15:03:18 +0100

    Update basicBot.js
    Gif command for manager

--- basicBot.js
+++ basicBot.js
@@ -2031,7 +2031,7 @@
 
             gifCommand: {
                 command: ['gif', 'giphy'],
-                rank: 'user',
+                rank: 'manager',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
